f2df4d17d469b30839dcbcfe99daab206e2188cc
Marion Licht <Marion Licht>
2017-05-08 15:50:52 +0200

    Mijn 1e poging tot een OCA oefening.

---
+++ src/nl/kadaster/oca/MyFirstOCAProgram.java
@@ -0,0 +1,11 @@
+package nl.kadaster.oca;
+
+import java.util.*;
+
+public class MyFirstOCAProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Mijn eerste OCA programma");
+	}
+
+}
