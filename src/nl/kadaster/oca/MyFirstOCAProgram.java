b70ccd1d7410f3b10979c0574dd265f785d10705
Marion Licht <Marion Licht>
2017-05-08 15:55:17 +0200

    Een toevoeging die ik net ontwikkeld heb.

--- src/nl/kadaster/oca/MyFirstOCAProgram.java
+++ src/nl/kadaster/oca/MyFirstOCAProgram.java
@@ -6,6 +6,9 @@ public class MyFirstOCAProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Mijn eerste OCA programma");
+		System.out.println("Een toevoeging die ik niet zo heel nuttig vind.");
+	
+	
 	}
 
 }
